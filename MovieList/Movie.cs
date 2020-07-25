a0554800aca67be3aeed061a6b5f2f64610bbb02
Randy Zilinski <rzilinski77@gmail.com>
2020-07-25 12:10:37 -0400

    Add project files.

---
+++ MovieList/Movie.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MovieList
+{
+    class Movie
+    {
+        private string Title { get; set; }
+        private string Category { get; set; }
+
+        public Movie() { }
+        public Movie (string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+        public List<string> GetCategory(string number, List<Movie> movieList)
+        {
+            List<string> sortedList = new List<string>();
+            bool again = true;
+
+            while (again == true)
+            {
+                if (number == "1" || number == "one")
+                {
+                    foreach (Movie movie in movieList)
+                    {
+                        if (movie.Category == "Horror")
+                        {
+                            sortedList.Add(movie.Title);
+                            again = false;
+                        }
+                    }
+                }
+                else if (number == "2" || number == "two")
+                {
+                    foreach (Movie movie in movieList)
+                    {
+                        if (movie.Category == "Animated")
+                        {
+                            sortedList.Add(movie.Title);
+                            again = false;
+                        }
+                    }
+                }
+                else if (number == "3" || number == "three")
+                {
+                    foreach (Movie movie in movieList)
+                    {
+                        if (movie.Category == "Scifi")
+                        {
+                            sortedList.Add(movie.Title);
+                            again = false;
+                        }
+                    }
+                }
+                else if (number == "4" || number == "four")
+                {
+                    foreach (Movie movie in movieList)
+                    {
+                        if (movie.Category == "Drama")
+                        {
+                            sortedList.Add(movie.Title);
+                            again = false;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid entry, please try again");
+                    Console.WriteLine("What Category are you interested in? Horror(1), Animated(2), Scifi(3), Drama(4)");
+                    Console.Write("Enter a Number: ");
+                    number = Console.ReadLine().ToLower();
+                }
+            }
+
+
+            sortedList.Sort();
+            return sortedList;
+
+        }
+
+
+    }
+}
---
+++ MovieList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie();
+            List<Movie> movieList = new List<Movie>();
+
+            movieList.Add(new Movie("Midsommar", "Horror"));
+            movieList.Add(new Movie("Hereditary", "Horror"));
+            movieList.Add(new Movie("The Grudge", "Horror"));
+            movieList.Add(new Movie("Whisper of the Heart", "Animated"));
+            movieList.Add(new Movie("My Neighbor Totoro", "Animated"));
+            movieList.Add(new Movie("Nausicaa: Valley of the Wind", "Animated"));
+            movieList.Add(new Movie("Sector 9", "Scifi"));
+            movieList.Add(new Movie("Chappie", "Scifi"));
+            movieList.Add(new Movie("Dirty Dancing", "Drama"));
+            movieList.Add(new Movie("Memiors of a Geisha", "Drama"));
+
+            Console.WriteLine("Welcome to the Movie List Application!\n");
+            int numberOfMovies = movieList.Count;
+            Console.WriteLine($"There are {numberOfMovies} movies in the list.");
+            Console.WriteLine("What Category are you interested in? Horror(1), Animated(2), Scifi(3), Drama(4)");
+            Console.Write("Enter a Number: ");
+            string number = Console.ReadLine().ToLower();
+            List<string> sortedList = movie.GetCategory(number, movieList);
+
+            foreach (string title in sortedList)
+            {
+                Console.WriteLine(title);
+            }
+
+        }
+
+
+    }
+}
